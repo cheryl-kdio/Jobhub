0f2bee17ce611f16d46f390c92e11af50acc0e90
JARRY <antoine.jarry@eleve.ensai.fr>
2023-09-10 22:57:02 +0200

    Modif sur memory_id et utilisateur 10/09 par Antoine

--- memory_id.py
+++ memory_id.py
@@ -1,16 +1,22 @@
-from abc import ABC
 
-class Memory(ABC):
-    def __init__(self):
-        self.dicti={}
+class Memory: # Création classe de stockage 
+    db={} # Utilisation d'un dictionnaire pour le stockage avec ID interne comme key 
         
-    def add_dict(self,id,value):
-        self.dicti.append[self.id]=self.value
+    def add_db(self,id,value):
+        self.db[id]=value # Fonction d'ajout à notre dictionnaire
 
     def drop_id(self,id):
-        self.dicti.pop(self.id)
-
-
+        self.db.pop(id) # Fonction de Suppression à notre dictionnaire 
+    def iterer_donnees(self):
+        return list(self.db.items())
+    
+    def recuperer_utilisateur(self, id):
+        # Récupère les données d'un utilisateur depuis la base de données partagée
+        return self.db.get(id)
+    
+    def afficher_db(self) : # Affiche l'ensemble de la base de données
+        for id, value in self.db.items():
+            print(f"id : {id}, Données : {value}")
 
 
         
--- utilisateur.py
+++ utilisateur.py
@@ -2,27 +2,28 @@ from abc import ABC,abstractmethod
 from random import randint
 from memory_id import Memory
 
-class Utilisateur(ABC) :
+class Utilisateur(Memory) : # Création classe Utilisateur
 
-        
-    def create_account(self):
-        id = randint(0,999999999999)
-        while id in Memory.dicti :
+    def create_account(self): # Création de la fonction créer un compte
+        id = randint(0,999999999999) # Attribution ID interne 
+        while id in Memory().iterer_donnees() : # Verif ID n'existe pas déjà dans notre stockage 
             id = randint(0,999999999999)
         
         name_user=input('nom utilisateur :')
         password='a'
-        password2='a'
+        password2='b'
         while password != password2:
             password=input("mot de passe :")
             password2=input("confirmation du mot de passe :")
 
-        Memory.dicti.add_dict(id,[name_user,password])
+        Memory().add_db(id,[name_user,password]) # Ajout de l'utilisateur à notre stockage 
 
-crea = Utilisateur()
-crea.create_account()
+u1 = Utilisateur()
+u2 = Utilisateur()
+u1.create_account()
+u2.create_account()
 
-
+Memory().afficher_db()
         
 
 
