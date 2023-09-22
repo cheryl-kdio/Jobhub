496164ddddb1f6280f45845ff0de6c3123ec5941
cherry-san <123456789+cheryl.s.kouadio@gmail.com>
2023-09-23 00:00:20 +0200

    Disjonction de la classe CompteUtilisateur en trois classes

--- business/dao/compte_utilisateur_dao.py
+++ business/dao/compte_utilisateur_dao.py
@@ -1,6 +1,5 @@
-# from dao.db_connection import DBConnection
-# from utils.singleton import Singleton
-
+from business.dao.db_connection import DBConnection
+from business import Singleton
 from business.client.compte_utilisateur import CompteUtilisateur
 
 
@@ -33,38 +32,38 @@ class CompteUtilisateurDao(metaclass=Singleton):
 
         return res > 0
 
-    # def update(self, attribut, attribut_contenu):
-    #     """Mise à jour des informations d'un utilisateur dans la base de données
+    def update(self, attribut, attribut_contenu, user):
+        """Mise à jour des informations d'un utilisateur dans la base de données
 
-    #     Parameters
-    #     ----------
-    #     attribut : str
-    #         attribut de l'utilisateur à mettre à jour dans la base de données
+        Parameters
+        ----------
+        attribut : str
+            attribut de l'utilisateur à mettre à jour dans la base de données
 
-    #     attribut_contenu : str
-    #         contenu de l'attribut de l'utilisateur à mettre à jour dans la base de données
+        attribut_contenu : str
+            contenu de l'attribut de l'utilisateur à mettre à jour dans la base de données
 
-    #     Returns
-    #     -------
-    #         True si l'utilisateur a bien été mis à jour
-    #     """
-    #     try:
-    #         with DBConnection().connection as connection:
-    #             with connection.cursor() as cursor:
-    #                 # Supprimer le compte d'un utilisateur
-    #                 cursor.execute(
-    #                     "UPDATE database.compte_utilisateur           "
-    #                     " SET %(attribut)s=%(attribut_contenu)s      "
-    #                     " WHERE id_compte_utilisateur=%(id_compte_utilisateur)s     ",  # creer db compte_utilisateur ou changer le nom...
-    #                     {
-    #                         "attribut": attribut,
-    #                         "attribut_contenu": attribut_contenu,
-    #                         "id_compte_utilisateur": user.id_compte_utilisateur,
-    #                     },
-    #                 )
-    #                 res = cursor.rowcount
-    #     except Exception as e:
-    #         print(e)
-    #         raise
+        Returns
+        -------
+            True si l'utilisateur a bien été mis à jour
+        """
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    # Supprimer le compte d'un utilisateur
+                    cursor.execute(
+                        "UPDATE database.compte_utilisateur           "
+                        " SET %(attribut)s=%(attribut_contenu)s      "
+                        " WHERE id_compte_utilisateur=%(id_compte_utilisateur)s     ",  # creer db compte_utilisateur ou changer le nom...
+                        {
+                            "attribut": attribut,
+                            "attribut_contenu": attribut_contenu,
+                            "id_compte_utilisateur": user.id_compte_utilisateur,
+                        },
+                    )
+                    res = cursor.rowcount
+        except Exception as e:
+            print(e)
+            raise
 
-    #     return res > 0
+        return res > 0
