16f5e3cfa8e1d1d51cdf17c64abed61b2a94f129
Antoine <antoine.jarry@eleve.ensai.fr>
2023-09-28 21:25:24 +0200

    Modif

--- Business_layer/Service_layer/utilisateur.py
+++ Business_layer/Service_layer/utilisateur.py
@@ -2,30 +2,86 @@ from abc import ABC, abstractmethod
 from random import randint
 from Persistance_layer.memory_id import Memory
 
+from getpass import getpass
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+
 
 class Utilisateur(Memory):  # Création classe Utilisateur
     def create_account(self):  # Création de la fonction créer un compte
-        id = randint(0, 999999999999)  # Attribution ID interne
+        id = randint(0, 9999999)  # Attribution ID interne
         while (
             id in Memory().iterer_donnees()
         ):  # Verif ID n'existe pas déjà dans notre stockage
-            id = randint(0, 999999999999)
+            id = randint(0, 9999999)
 
         name_user = input("nom utilisateur :")
-        password = "a"
-        password2 = "b"
-        while password != password2:
-            password = input("mot de passe :")
-            password2 = input("confirmation du mot de passe :")
+        passw = getpass("Mot de passe : ")
+        password = ph.hash(passw)
+
+        password_to_check = getpass("Vérification du mot de passe : ")
+        try:
+            ph.verify(password, password_to_check)
+            print("Le mot de passe est valide.")
+        except:
+            print("Le mot de passe est invalide.")
 
         Memory().add_db(
-            id, [name_user, password]
+            id, name_user, password
         )  # Ajout de l'utilisateur à notre stockage
+
+    def se_connecter(self):
+        try:
+            # Connexion à la base de données PostgreSQL
+            connexion = psycopg2.connect(
+                database="votre_base_de_donnees",
+                user="votre_utilisateur",
+                password="votre_mot_de_passe",
+                host="votre_hôte",
+                port="votre_port",
+            )
+
+            # Création d'un curseur pour exécuter des requêtes SQL
+            curseur = connexion.cursor()
+
+            # Demander à l'utilisateur de saisir son identifiant et son mot de passe
+            identifiant = input("Identifiant : ")
+            mot_de_passe = input("Mot de passe : ")
+
+            # Requête SQL pour vérifier les informations de connexion
+            requete = (
+                "SELECT * FROM utilisateurs WHERE identifiant=%s AND mot_de_passe=%s"
+            )
+            curseur.execute(requete, (identifiant, mot_de_passe))
+
+            # Récupérer le résultat de la requête
+            utilisateur = curseur.fetchone()
+
+            # Fermer le curseur et la connexion à la base de données
+            curseur.close()
+            connexion.close()
+
+            # Vérifier si l'utilisateur a été trouvé dans la base de données
+            if utilisateur is not None:
+                print(
+                    "Connexion réussie ! Bienvenue,", utilisateur[1]
+                )  # Supposons que le nom de l'utilisateur est dans la deuxième colonne
+            else:
+                print("Identifiant ou mot de passe incorrect. Veuillez réessayer.")
+
+        except Exception as e:
+            print("Erreur lors de la connexion à la base de données :", e)
+
+
+# Appeler la fonction se_connecter
+if __name__ == "__main__":
+    se_connecter()
 
 
 u1 = Utilisateur()
-u2 = Utilisateur()
 u1.create_account()
-u2.create_account()
+
+Memory().drop_id(8786055)
 
 Memory().afficher_db()
--- Persistance_layer/memory_id.py
+++ Persistance_layer/memory_id.py
@@ -1,19 +1,70 @@
-class Memory:  # Création classe de stockage
-    db = {}  # Utilisation d'un dictionnaire pour le stockage avec ID interne comme key
+import psycopg2
+from Persistance_layer.db_connection import DBConnection
 
-    def add_db(self, id, value):
-        self.db[id] = value  # Fonction d'ajout à notre dictionnaire
+
+class Memory:  # Création classe de stockage # Peut-être dans DAO layer ?
+    def add_db(self, id, name_user, password):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                insert_sql = (
+                    "insert into account (ID, name_user, password) values(%s, %s, %s)"
+                )
+                values = (id, name_user, password)
+                cur.execute(insert_sql, values)
 
     def drop_id(self, id):
-        self.db.pop(id)  # Fonction de Suppression à notre dictionnaire
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                delete_line = f"DELETE FROM account WHERE ID = %s;"
+                cur.execute(delete_line, (id,))
 
     def iterer_donnees(self):
-        return list(self.db.items())
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                get_id = f"SELECT ID FROM account;"
+                cur.execute(get_id)
+                id_liste = cur.fetchall()
+        return [id_liste[0] for id in id_liste]
 
     def recuperer_utilisateur(self, id):
         # Récupère les données d'un utilisateur depuis la base de données partagée
         return self.db.get(id)
 
     def afficher_db(self):  # Affiche l'ensemble de la base de données
-        for id, value in self.db.items():
-            print(f"id : {id}, Données : {value}")
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                display_all = f"SELECT * FROM account;"
+                cur.execute(display_all)
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                get_id = f"SELECT ID FROM account;"
+                cur.execute(get_id)
+                id_liste = cur.fetchall()
+        return [id_liste[0] for id in id_liste]
+
+    def add_perso(
+        self,
+        id,
+        nom=None,
+        date_naissance=None,
+        age=None,
+        mail=None,
+        tel=None,
+        ville=None,
+        code_postal=None,
+    ):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cur:
+                update = """UPDATE account
+                SET nom=COALESCE(%s,nom), date_naissance =COALESCE(%s,date_naissance),
+                    age = COALESCE(%s,age), mail = COALESCE(%s,mail),
+                    tel = COALESCE(%s,tel), ville = COALESCE(%s,ville), code_postal=COALESCE(%s,code_postal)
+                WHERE id = %s """
+                cur.execute(
+                    update,
+                    (nom, date_naissance, age, mail, tel, ville, code_postal, id),
+                )
