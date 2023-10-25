7f89205f6bd700e2dbfa6e955bd77073e9548741
bgueraud <ben.gueraud@yahoo.com>
2023-10-25 09:56:44 +0200

    connexion, env

--- business/services/utilisateur_service.py
+++ business/services/utilisateur_service.py
@@ -1,10 +1,12 @@
 from business.dao.utilisateur_dao import UtilisateurDao
-
+import argon2
 
 from getpass import getpass
 from argon2 import PasswordHasher
 
 ph = PasswordHasher()
+import random
+import string
 
 
 class Utilisateur(UtilisateurDao):  # Création classe Utilisateur
@@ -12,17 +14,27 @@ class Utilisateur(UtilisateurDao):  # Création classe Utilisateur
         name_user = input("nom utilisateur :")
         mail = input("Adresse e_mail")
         passw = getpass("Mot de passe : ")
-        password = ph.hash(passw)
-
         password_to_check = getpass("Vérification du mot de passe : ")
+
+        sel = "".join(
+            random.choice(string.ascii_letters + string.digits) for _ in range(16)
+        )
+
+        mot_de_passe_concatene = sel + passw
+
+        password = ph.hash(mot_de_passe_concatene)
+        print(password)
+        print(ph.hash(sel + password_to_check))
+
         try:
-            ph.verify(password, password_to_check)
+            mot_de_passe_concatene_to_check = sel + password_to_check
+            ph.verify(password, mot_de_passe_concatene_to_check)
             print("Le mot de passe est valide.")
         except:
             print("Le mot de passe est invalide.")
 
         UtilisateurDao().add_db(
-            id, name_user, mail, password
+            name_user, mail, password, sel
         )  # Ajout de l'utilisateur à notre stockage
 
     def se_connecter(self):  # Voir comment l'ajuster avec notre
@@ -33,9 +45,9 @@ class Utilisateur(UtilisateurDao):  # Création classe Utilisateur
             utilisateur = UtilisateurDao().verif_connexion(mail=mail)
 
             # Vérifier si l'utilisateur a été trouvé dans la base de données
-            if utilisateur is not None:
+            if utilisateur:
                 print(
-                    "Connexion réussie ! Bienvenue,", utilisateur[1]
+                    "Connexion réussie ! Bienvenue,"
                 )  # Supposons que le nom de l'utilisateur est dans la deuxième colonne
             else:
                 print("Identifiant ou mot de passe incorrect. Veuillez réessayer.")
@@ -47,6 +59,5 @@ class Utilisateur(UtilisateurDao):  # Création classe Utilisateur
 # Appeler la fonction se_connecter
 if __name__ == "__main__":
     u1 = Utilisateur()
-    u1.create_account()
-
-    UtilisateurDao().afficher_db()
+    UtilisateurDao().drop_id(17)
+    u1.se_connecter()
