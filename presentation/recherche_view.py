407ec0d813eb7afbb220ab8b9cd1bc6a04b23cbc
cherry-san <123456789+cheryl.s.kouadio@gmail.com>
2023-11-23 22:46:41 +0100

    maj rechercheview

--- presentation/recherche_view.py
+++ presentation/recherche_view.py
@@ -6,22 +6,22 @@ from business.services.utilisateur_service import (
     Utilisateur,
 )  # Importation de la classe Utilisateur depuis le module utilisateur_service
 from business.dao.utilisateur_dao import UtilisateurDao
-from business.business_objectss_object.recherche import Recherche
+from business.business_object.recherche import Recherche
 from business.services.recherche_service import RechercheService
 from tabulate import tabulate
 from business.dao.profil_chercheur_emploi_dao import ProfilChercheurEmploiDao
-from business.business_objectss_object.profil_chercheur_emploi import ProfilChercheurEmploi
+from business.business_object.profil_chercheur_emploi import ProfilChercheurEmploi
 from business.dao.recherche_dao import RechercheDao
 
 from business.dao.candidature_dao import CandidatureDao
 
 
 class RechercheView(AbstractView):
-    def __init__(self, langue, query_params,user=None):
+    def __init__(self, langue, query_params={}, user=None):
         self.langue = langue
         self.user = user
         self.results_per_page = 20
-        self.query_params=query_params
+        self.query_params = query_params
         self.query_params["results_per_page"] = self.results_per_page
         self.__questions = [
             {
@@ -36,11 +36,13 @@ class RechercheView(AbstractView):
         ]
 
     def make_choice(self):
-        if self.query_params is None:
+        if len(self.query_params) == 1:
             answers = prompt(self.__questions)
-            self.query_params = {
-                "what": answers["mot_cle"].replace(",", " "),
-            }
+            self.query_params = self.query_params.update(
+                {
+                    "what": answers["mot_cle"].replace(",", " "),
+                }
+            )
 
         recherche = Recherche(query_params=self.query_params)
         r = RechercheService()
@@ -234,8 +236,4 @@ class RechercheView(AbstractView):
                     return UserView(user=self.user, langue=self.langue)
 
     def display_info(self):
-        print(
-            "Veuillez entrer les informations suivantes :"
-            if self.langue == "français"
-            else "Please enter the following information:"
-        )
+        pass
