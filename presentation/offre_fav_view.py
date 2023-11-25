b801042b1af004dfc66c019d477c7463a683730d
Antoine <antoine.jarry@eleve.ensai.fr>
2023-11-25 16:29:30 +0100

    dernière modif

--- presentation/offre_fav_view.py
+++ presentation/offre_fav_view.py
@@ -3,6 +3,7 @@ from presentation.abstract_view import AbstractView
 from presentation.session import Session
 from business.dao.offre_dao import OffreDao
 
+
 class OffreView(AbstractView):
     def __init__(self, user, langue):
         self.user = user
@@ -30,27 +31,28 @@ class OffreView(AbstractView):
                 else "You don't have any saved offers."
             )
 
-            self.__questions=[
-                    {
-                        "type": "confirm",
-                        "name": "oui",
-                        "message": "Souhaiter vous lancer une recherche ?"
-                        if self.langue == "français"
-                        else "Do you want to start a reasearch ? ",
-                        "default": True,
-                    }
-                ]
+            self.__questions = [
+                {
+                    "type": "confirm",
+                    "name": "oui",
+                    "message": "Souhaiter vous lancer une recherche ?"
+                    if self.langue == "français"
+                    else "Do you want to start a reasearch ? ",
+                    "default": True,
+                }
+            ]
 
-            answers=prompt(self.__questions)
+            answers = prompt(self.__questions)
 
-            if answers['oui']:
-
+            if answers["oui"]:
                 from presentation.recherche_view import RechercheView
+
                 return RechercheView(langue=self.langue, user=self.user)
             else:
                 from presentation.user_view import UserView
-                return UserView(user=self.user, langue=self.langue) 
-                
+
+                return UserView(user=self.user, langue=self.langue)
+
         else:
             message_fr = "Choisissez une offre à détailler : \n - - - - - - - - - - - - - - - - - - - -\n"
             message_en = "Choose a job to view in detail."
@@ -65,6 +67,7 @@ class OffreView(AbstractView):
             answers = prompt(self.__questions)
             if answers[0] in ("Retour", "Return"):
                 from presentation.user_view import UserView
+
                 return UserView(self.user, self.langue)
             else:
                 print(answers[0])
@@ -79,41 +82,45 @@ class OffreView(AbstractView):
                     }
                 ]
                 answers2 = prompt(self.__questions)
-                if answers2['oui']:
-                    if o.deja_candidat(answers[0],self.user):
+                if answers2["oui"]:
+                    if o.deja_candidat(answers[0], self.user):
                         print("Vous avez dejà envoyé votre candidature")
                         return OffreView(user=self.user, langue=self.langue)
 
                     else:
-                        o.candidater(answers[0],self.user)
+                        o.candidater(answers[0], self.user)
                         message_fr = "Votre candidature a bien été envoyée!"
                         message_en = "Your submission was sent !"
-                        message = message_fr if self.langue == "français" else message_en
+                        message = (
+                            message_fr if self.langue == "français" else message_en
+                        )
                         print(message)
                         return OffreView(user=self.user, langue=self.langue)
-                else:
-                    from presentation.user_view import UserView
-                    return UserView(user=self.user, langue=self.langue)
 
-
-                self.__questions = [
-                    {
-                        "type": "confirm",
-                        "name": "oui",
-                        "message": "Souhaitez-vous supprimer l'offre de vos favoris ?"
-                        if self.langue == "français"
-                        else "Delete the offer from your favorites?",
-                        "default": True,
-                    }
-                ]
-                answers2 = prompt(self.__questions)
-                if answers2['oui']:
-                    o.supprimer_offre(answers[0])
-                    message_deleted_fr = "L'offre a bien été supprimée"
-                    message_deleted_en = "The offer has been deleted"
-                    message_deleted = message_deleted_fr if self.langue == "français" else message_deleted_en
-                    print(message_deleted)
-                    return OffreView(user=self.user, langue=self.langue)
                 else:
-                    from presentation.user_view import UserView
-                    return UserView(user=self.user, langue=self.langue)
+                    self.__questions = [
+                        {
+                            "type": "confirm",
+                            "name": "oui",
+                            "message": "Souhaitez-vous supprimer l'offre de vos favoris ?"
+                            if self.langue == "français"
+                            else "Delete the offer from your favorites?",
+                            "default": True,
+                        }
+                    ]
+                    answers2 = prompt(self.__questions)
+                    if answers2["oui"]:
+                        o.supprimer_offre(answers[0])
+                        message_deleted_fr = "L'offre a bien été supprimée"
+                        message_deleted_en = "The offer has been deleted"
+                        message_deleted = (
+                            message_deleted_fr
+                            if self.langue == "français"
+                            else message_deleted_en
+                        )
+                        print(message_deleted)
+                        return OffreView(user=self.user, langue=self.langue)
+                    else:
+                        from presentation.user_view import UserView
+
+                        return UserView(user=self.user, langue=self.langue)
